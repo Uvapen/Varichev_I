8fd2dd4bb9e22367a817887c8e4f235dd70d368a
Varichev_I <iluxa.bagarik@gmail.com>
2020-11-05 22:45:49 +0400

    2.3. Layout: верстка макета

--- 01.Основы Html/test.js
+++ 01.Основы Html/test.js
@@ -1,8 +1,9 @@
 var counter = 1;
+var itemCounter = 0;
 
 function updateCounter() {
-    document.querySelector(".button2").style. background = "orange";
-    document.querySelector(".button2").innerText = counter;
+    document.querySelector(".button-counter").style. background = "orange";
+    document.querySelector(".button-counter").innerText = counter;
     counter++;
 }
 
@@ -30,6 +31,7 @@ overlay.addEventListener('click', () => {
         closeModel(model);
     })
 })
+
 function openModel(model) {
     if (model == null) return
     model.classList.add('active');
@@ -46,26 +48,25 @@ function findItems() {
     var addToCartButton = document.querySelectorAll('[cart-add-button]');
     addToCartButton.forEach(button => {
         button.addEventListener('click', () => {
-        var shopItem = button.parentElement.parentElement;
+        var shopItem = button.parentElement;
         var title = shopItem.querySelector('.title').innerText;
         var price = shopItem.querySelector('.price').innerText;
         var img = shopItem.querySelector('.item-img').src;
         addItemToCart (title, price, img);
         console.log(title, price, img);
         })
-        
+    
     })
      
 }
 
 function addItemToCart (title, price, img) {
     var cartRow = document.createElement('div');
-    var cartItems = document.getElementsByClassName('cart-items')[0];
-    var cartContent = `<div class="item">
-                                <img class="cart-img" src=${img}>
-                                <div class="title">Naruto Hoodies Men 3D Printed</div>
-                                <div class="price">2500р</div>  
-                            </div>`
+    var cartItems = document.getElementsByClassName('cart-item')[0];
+    var cartContent = ` <img class="cart-img" src="${img}">
+                        <span class="cart-title">${title}</span>
+                        <span class= "cart-price">${price}</span>  
+                        `
     cartRow.innerHTML = cartContent;
     cartItems.append(cartRow);
 }
